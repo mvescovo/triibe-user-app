44e7637e69a9a5be2cedeae7b9fd09201b71e0fb
Michael <m.vescovo@gmail.com>
2016-07-27 13:51:55 +1000

    feat: Add sign out

--- app/src/main/java/com/example/triibe/triibeuserapp/trackData/TrackDataActivity.java
+++ app/src/main/java/com/example/triibe/triibeuserapp/trackData/TrackDataActivity.java
@@ -1,21 +1,40 @@
 package com.example.triibe.triibeuserapp.trackData;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.triibe.triibeuserapp.R;
+import com.example.triibe.triibeuserapp.auth.AuthUiActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TrackDataActivity extends AppCompatActivity {
     private static final String TAG = "TrackDataActivity";
+    @BindView(android.R.id.content)
+    View mRootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_data);
+        ButterKnife.bind(this);
 
         displayProfileInfo();
     }
@@ -41,5 +60,52 @@ public class TrackDataActivity extends AppCompatActivity {
             Log.i(TAG, "displayProfileInfo: photoUrl: " + photoUrl);
             Log.i(TAG, "displayProfileInfo: uid: " + uid);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.track_data_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*
+    * Sign the user out using the Firebase UI authentication flow.
+    * https://github.com/firebase/FirebaseUI-Android/tree/master/auth
+    *
+    * Note: of the 3 signin components (Firebase, Google signin, Google smartlock), it seems the
+    * Google signin is not signing out and still auto logs in. Will need further testing.
+    * */
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), AuthUiActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.sign_out_failed);
+                        }
+                    }
+                });
+    }
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG)
+                .show();
     }
 }
